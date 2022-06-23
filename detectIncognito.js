67ec5782afd54326ebbd98ec867ba5e2f5295143
Joe Rutkowski <Joe@dreggle.com>
2022-06-23 10:44:00 -0400

    safari/chrome for iOS fix
    New single Safari method added that works on both macOS and iOS (and Chrome for iOS)

--- detectIncognito.js
+++ detectIncognito.js
@@ -1,19 +1,19 @@
 /**
  *
- * detectIncognito v1.0.0 - (c) 2022 Joe Rutkowski <Joe@dreggle.com> (https://github.com/Joe12387/detectIncognito)
+ * detectIncognito v1.1.0b - (c) 2022 Joe Rutkowski <Joe@dreggle.com> (https://github.com/Joe12387/detectIncognito)
  *
  **/
 var detectIncognito = function() {
   return new Promise(function(resolve, reject) {
     var browserName = "Unknown";
-  
+
     function __callback(isPrivate) {
       resolve({
         isPrivate: isPrivate,
         browserName: browserName
       });
     }
-    
+
     function identifyChromium() {
       var ua = navigator.userAgent;
       if (ua.match(/Chrome/)) {
@@ -29,60 +29,56 @@ var detectIncognito = function() {
         return "Chromium";
       }
     }
-    
+
     function assertEvalToString(value) {
       return value === eval.toString().length;
     }
-    
+
     function isSafari() {
       var v = navigator.vendor;
       return v !== undefined && v.indexOf("Apple") === 0 && assertEvalToString(37);
     }
-  
+
     function isChrome() {
       var v = navigator.vendor;
       return v !== undefined && v.indexOf("Google") === 0 && assertEvalToString(33);
     }
-  
+
     function isFirefox() {
       return document.documentElement !== undefined && document.documentElement.style.MozAppearance !== undefined && assertEvalToString(37);
     }
-  
+
     function isMSIE() {
       return navigator.msSaveBlob !== undefined && assertEvalToString(39);
     }
-  
+
     /**
      * Safari (Safari for iOS & macOS)
      **/
-  
-    function macOS_safari14() {
+
+    function newSafariTest() {
       try {
-        window.safari.pushNotification.requestPermission("https://example.com", "private", {}, (function() {}));
-      } catch (e) {
-        return __callback(!new RegExp("gesture").test(e));
-      }
-      return __callback(false);
-    }
-  
-    function iOS_safari14() {
-      var tripped = false;
-      var iframe = document.createElement("iframe");
-      iframe.style.display = "none";
-      document.body.appendChild(iframe);
-  
-      iframe.contentWindow.applicationCache.addEventListener("error", function() {
-        tripped = true;
-        return __callback(true);
-      });
-  
-      setTimeout(function() {
-        if (!tripped) {
-          __callback(false);
+        var db = window.indexedDB.open("test", 1);
+        db.onupgradeneeded = function(i) {
+          var res = db.target.result;
+          try {
+            res.createObjectStore("test", {
+              autoIncrement: true
+            }).put(new Blob);
+            __callback(false);
+          } catch (e) {
+            if (/BlobURLs are not yet supported/.test(e.message)) {
+              __callback(true);
+            } else {
+              __callback(false);
+            }
+          }
         }
-      }, 100);
+      } catch (e) {
+        __callback(false);
+      }
     }
-  
+
     function oldSafariTest() {
       var openDB = window.openDatabase;
       var storage = window.localStorage;
@@ -99,29 +95,19 @@ var detectIncognito = function() {
       }
       return __callback(false);
     }
-  
+
     function safariPrivateTest() {
-      var w = window;
       if (navigator.maxTouchPoints !== undefined) {
-        if (w.safari !== undefined && w.DeviceMotionEvent === undefined) {
-          browserName = "Safari for macOS";
-          macOS_safari14();
-        } else if (w.DeviceMotionEvent !== undefined) {
-          browserName = "Safari for iOS";
-          iOS_safari14();
-        } else {
-          reject(new Error("detectIncognito Could not identify this version of Safari"));
-        }
+        newSafariTest();
       } else {
-        browserName = "Safari";
         oldSafariTest();
       }
     }
-  
+
     /**
      * Chrome
      **/
-  
+
     function getQuotaLimit() {
       var w = window;
       if (w.performance !== undefined && w.performance.memory !== undefined && w.performance.memory.jsHeapSizeLimit !== undefined) {
@@ -129,7 +115,7 @@ var detectIncognito = function() {
       }
       return 1073741824;
     }
-  
+
     // >= 76
     function storageQuotaChromePrivateTest() {
       navigator.webkitTemporaryStorage.queryUsageAndQuota(
@@ -141,7 +127,7 @@ var detectIncognito = function() {
         }
       );
     }
-  
+
     // 50 to 75
     function oldChromePrivateTest() {
       var fs = window.webkitRequestFileSystem;
@@ -153,7 +139,7 @@ var detectIncognito = function() {
       };
       fs(0, 1, success, error);
     }
-  
+
     function chromePrivateTest() {
       if (Promise !== undefined && Promise.allSettled !== undefined) {
         storageQuotaChromePrivateTest();
@@ -161,25 +147,26 @@ var detectIncognito = function() {
         oldChromePrivateTest();
       }
     }
-  
+
     /**
      * Firefox
      **/
-  
+
     function firefoxPrivateTest() {
       __callback(navigator.serviceWorker === undefined);
     }
-  
+
     /**
      * MSIE
      **/
-  
+
     function msiePrivateTest() {
       __callback(window.indexedDB === undefined);
     }
-  
+
     function main() {
       if (isSafari()) {
+        browserName = "Safari";
         safariPrivateTest();
       } else if (isChrome()) {
         browserName = identifyChromium();
@@ -194,7 +181,7 @@ var detectIncognito = function() {
         reject(new Error("detectIncognito cannot determine the browser"));
       }
     }
-  
+
     main();
   });
 };
